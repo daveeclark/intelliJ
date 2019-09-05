99958fa4d868f338f49d6a37b775541afddda86f
daveeclark <dclark2016@hotmail.com>
2019-09-05 07:13:11 -0400

    First commit

---
+++ yeerrrrrrr/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /*
+    This is the first line of a comment
+    This is the second line of a comment
+     // line comment
+     */
+    public static void main(String[] args) {
+        // this is a stand alone comment
+        System.out.println("what's up brodie one"); //This is a comment
+        //System.out.println("what's up brodie two");
+        System.out.println(/*"what's up brodie three"*/"This is different text");
+    }
+}
